e095c92dc427a74ac392c960b692dc328730215e
Marcelo Fonseca <projetosweb11@gmail.com>
2023-09-15 18:59:33 -0300

    exercicio de javaScript

---
+++ 07-modulo-css-avancado/exercicio.js
@@ -0,0 +1,44 @@
+//criar um programa que mostre numeros impares
+
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+    
+}
+
+{
+    console.log(2)
+    console.log(4)
+    console.log(6)
+    console.log(8)
+    console.log(10)
+}
+
+
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+let  precofinal = preco + imposto;
+    console.log("caneta");
+    console.log(quantidade);
+    console.log(preco);
+    console.log(imposto);
+    console.log(precofinal);
+
+
+    ///****************
+    
+let precos = 19.90;
+let desconto = 0.4;
+let precoComDesconto = precos * (1 - desconto);
+console.log(precoComDesconto);
+
+const PI = 3.141592;
+const raio = 10;
+let areaCirc = PI * raio * raio;
+areaCirc = Math.PI * raio *raio;
+console.log("O valor da area é: " + areaCirc + "m2.");
+
