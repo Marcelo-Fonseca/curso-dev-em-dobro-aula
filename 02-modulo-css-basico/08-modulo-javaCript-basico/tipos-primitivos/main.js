b0969c81adc97f3a4502b0c82d1a4cbaffffb1c0
Marcelo Fonseca <projetosweb11@gmail.com>
2024-03-17 14:32:10 -0300

    atualizalçao de arquivos

---
+++ 02-modulo-css-basico/08-modulo-javaCript-basico/tipos-primitivos/main.js
@@ -0,0 +1,15 @@
+/*tipo primitivo String*/
+
+let nome = 'Marcelo'
+let numero = 1230
+let usuarioVerificado = false
+
+if(usuarioVerificado) {
+    console.log('usuario verificado')
+}else{
+    console.log('usuario não verificado')
+}
+
+let pessoa = null /*no null ele nao recebe nada na memoria é uma variavel vazia*/ 
+/* tipo undefined é um tipo idefinido */
+
