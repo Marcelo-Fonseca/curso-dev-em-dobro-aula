5fc8ee518eb8b2cc2d51489ffb88787341b3f94f
Marcelo Fonseca <projetosweb11@gmail.com>
2024-03-18 17:01:26 -0300

    operadores matematicos

---
+++ 02-modulo-css-basico/08-modulo-javaCript-basico/operadores-matematicos/js/main.js
@@ -0,0 +1,10 @@
+//soma de variaveis
+let primeiroValor = 4
+let segundoValor = 2
+
+primeiroValor + segundoValor
+console.log(primeiroValor + segundoValor)
+console.log(primeiroValor - segundoValor)
+console.log(primeiroValor / segundoValor)
+console.log(primeiroValor * segundoValor)
+console.log(primeiroValor % segundoValor)
