18da0e2e490b01b2f060cc8e8a2477757fb90ed7
Marcelo Fonseca <projetosweb11@gmail.com>
2024-03-19 13:05:07 -0300

    atualização de arquivos e pasta e arquivos criados operadores logicos while

---
+++ 02-modulo-css-basico/08-modulo-javaCript-basico/lacosDeRepeticaoLoops/while/js/while.js
@@ -0,0 +1,21 @@
+//while = uaiu = enquanto essa condição for verdadeira execute o bloco de codigo, se for false sai do while. 
+//while(condição){bloco de execução}
+let a =10
+let b= 26
+while(a < b){
+    console.log('incrementando a variavel a' + a)
+    a++;
+
+}
+
+// do while = laço de repetição com teste logico no final 
+//do {
+//bloco de execução
+//}while(condição)
+
+var i = 1
+do{
+    console.log('entrou ' + 1)
+    i++
+}while(i <= 5)
+
