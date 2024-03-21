f56c331b88d51b95369b934c1ada3e26d805ed38
Marcelo Fonseca <projetosweb11@gmail.com>
2024-03-21 12:06:03 -0300

    modulo condionais e arquivos html js com exercicio de if e else e operador ternario

---
+++ 02-modulo-css-basico/08-modulo-javaCript-basico/condicionais/js/condicionais.js
@@ -0,0 +1,21 @@
+//condicionais
+// se
+
+let nomeFilme = 'vingadores'
+
+if(nomeFilme ==='Vingadores') {
+    console.log('É o filme dos vingadores')
+}else if(nomeFilme === 'Batman x Superman'){
+    console.log('É o filme do Batman x Superman')
+}else if(nomeFilme ==='capitao america') {
+    console.log('É o filmes do capitão America')
+}else {
+    console.log('É outro filme.')
+}
+
+
+//operador ternario
+//condição ? console.log(true): consoles.log(false)
+true ? console.log('Condição Verdadeira') : console.log('condição falsa')
+
+nomeFilme === 'Vingadores'? console.log('É o filme dos Vingadores') : console.log('É o outro filme')
