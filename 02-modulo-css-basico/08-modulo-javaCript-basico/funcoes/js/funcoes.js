f121cfd6a0ab18bceafdc9dbc80b527e31726636
Marcelo Fonseca <projetosweb11@gmail.com>
2024-03-24 13:05:17 -0300

    atualização

---
+++ 02-modulo-css-basico/08-modulo-javaCript-basico/funcoes/js/funcoes.js
@@ -0,0 +1,37 @@
+// Funçoes
+//Criar um escopo para executar um determinado codigo, pode ter dunções que retorne um valor no final ou funçoes que não retorne nada.
+
+//function nomeDafuncao(){
+    //<bloco de codigo>
+//}
+
+//chamar a função
+
+//nomeDafuncao() chamar a funcao é sempre fora das chaves.
+
+
+//funcoes com parametros
+///function nomeDafuncao( parametro1, parametro2){
+    //<bloco de codigo>
+//}
+
+//chamar a função
+
+//nomeDafuncao(parametro1, parametro2) chamar a funcao é sempre fora das chaves.
+//obs os parametretos sao separados por virgulas.
+
+function incetivarQuester(nomeQuester = 'Quester')
+{
+    alert('Muito bem' + nomeQuester +  ', Você chegou ao js, continue assim! ')
+}
+incetivarQuester('Marcelo')
+
+
+//funcao com parametro 
+// function soma(numero1, numero2){
+//     return numero1 + numero2
+
+// }
+// let resultado = soma(1,3)
+// console.log(resultado)
+// //soma(1,2)//assinatura da função
