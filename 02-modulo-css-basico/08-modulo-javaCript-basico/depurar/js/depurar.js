1b67d557cf8837ab0e5f8679d7dcac1dc730bdc3
Marcelo Fonseca <projetosweb11@gmail.com>
2024-03-25 10:19:10 -0300

    teste

---
+++ 02-modulo-css-basico/08-modulo-javaCript-basico/depurar/js/depurar.js
@@ -0,0 +1,22 @@
+//derar codigo no JS
+
+function calcularFatorial(numero) {
+    let resultado = 1;
+    for(let i = 1; i <= numero; i++) {
+        resultado *=i;
+        //resultado = resultado * i;
+
+
+    }
+    return resultado;
+
+
+}
+
+function imprimirFatorial(numero) {
+    const fatorial = calcularFatorial(numero);
+  
+  // console.log(´${numero} ${fatorial}´);
+}
+const numero = 5
+imprimirFatorial(numero);
